6511ce4079aab431617fc6d040676f30a4a2ee90
RidloSuhardi-1 <ridlosuhardi12@gmail.com>
2023-05-31 08:34:42 +0700

    perbaiki pembuatan db

--- lib/data/repository/setup_repo.dart
+++ lib/data/repository/setup_repo.dart
@@ -6,30 +6,7 @@ class SetupRepo {
   final _ref = FirebaseDatabase.instance.ref();
 
   void getSetup() {
-    _ref.once().then((DatabaseEvent event) async {
-      final snapshot = event.snapshot;
-
-      if (!snapshot.exists) {
-        final datetime = await getCurrentDateTime();
-
-        await _ref.set({
-          "configuration": {
-            "farthest_distance_sensor": 100,
-            "nearest_distance_sensor": 50,
-            "servo_enabled": true,
-            "servo_max_degree": 180,
-            "servo_min_degree": 90,
-            "servo_pause_time": 0,
-          },
-          "distance": {
-            "cm": 0.0,
-            "inch": 0.0,
-          },
-          "led_status": {"date": datetime, "status": false},
-          "system_notification": {},
-        });
-      }
-    });
+    firebaseInit();
   }
 
   void firebaseInit() {
@@ -38,6 +15,7 @@ class SetupRepo {
 
       if (!snapshot.exists) {
         final datetime = await getCurrentDateTime();
+        final datetimeSplit = datetime.split(' ');
 
         await _ref.set({
           "configuration": {
@@ -53,7 +31,14 @@ class SetupRepo {
             "inch": 0.0,
           },
           "led_status": {"date": datetime, "status": false},
-          "system_notification": {},
+          "system_notification": {
+            datetimeSplit[0]: {
+              datetimeSplit[1]: {
+                "message": "Database ditambahkan",
+                "status": "initial",
+              }
+            }
+          },
         });
       }
     });
